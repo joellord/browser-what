19dfc8032f757e390d49ffbf79f8340f4087f7ad
Joel Lord <joelphy@gmail.com>
2019-09-23 09:41:49 -0400

    Initial

---
+++ slave/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <title>My Browser Does What?</title>
+</head>
+
+<body>
+  <h1>My Browser Does What?</title>
+  <h3>iJS - Munich - Oct 2019</h3>
+
+  <div class="app">
+
+  </div>
+</body>
+
+<script type="text/javascript">
+
+</script>
+
+</html>
