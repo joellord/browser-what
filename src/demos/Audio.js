99fc9fb8f9a680d7f1b438a8c311b32ede36ce93
Joel Lord <joelphy@gmail.com>
2019-10-01 09:08:54 -0400

    Added files

---
+++ src/demos/Audio.js
@@ -0,0 +1,121 @@
+import React from "react";
+import { slidesGeneration } from "../utils/SlidesGenerator"; 
+
+const name = "Web Audio API";
+const demoName = "audio";
+const details = [
+  "Takes and audio stream and manipulates it",
+  "You can modulate the sound with oscillators"
+];
+const codeSamples = [
+  {
+    title: "Web Audio API", 
+    code: `
+let audioCtx = new AudioContext();
+let oscillator = audioCtx.createOscillator();
+let gainNode = audioCtx.createGain();
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+  `},  
+  {
+    title: "Change the frequency", 
+    code: `
+oscillator.frequency.value = 2000;
+  `},
+  {
+    title: "Change the volume (gain)",
+    code: `
+gainNode.gain.value = 0.1;
+  `}
+];
+
+const demo = {
+  init: (ctx) => {
+    ctx.setState({
+      freq: 2000,
+      gain: 1
+    });
+  },
+  render: (ctx) => {
+    const WIDTH = 300;
+    const HEIGHT = 300;
+    let audioCtx = new AudioContext();
+    let oscillator = audioCtx.createOscillator();
+    let analyzer = audioCtx.createAnalyser();
+    oscillator.connect(analyzer);
+    analyzer.connect(audioCtx.destination);
+    oscillator.frequency.value = 440;
+    let isPlaying = false;
+    analyzer.fftSize = 2048;
+    let bufferLength = analyzer.frequencyBinCount;
+    let dataArray = new Uint8Array(bufferLength);
+    let canvas, canvasCtx;
+    setTimeout(() => {
+      canvas = document.querySelector("#audioCanvas");
+      canvasCtx = canvas.getContext("2d");
+      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+      drawWave();
+    }, 0);
+    const handleOscillatorType = (e) => {
+      oscillator.type = e.target.value;
+    }
+    const handleFreqChange = (e) => {
+      if (!isPlaying) {
+        oscillator.start();
+        isPlaying = true;
+      }
+      oscillator.frequency.value = e.target.getAttribute("data-freq");
+    }
+    const handleStop = () => {
+      oscillator.stop();
+    };
+    const drawWave = () => {
+      setTimeout(drawWave, 100);
+      analyzer.getByteTimeDomainData(dataArray);
+      canvasCtx.fillStyle = "rgb(200, 200, 200)";
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+      canvasCtx.beginPath();
+      let sliceWidth = WIDTH * 2.5 / bufferLength;
+      let x = 0;
+      for (let i = 0; i < bufferLength; i++) {
+        let v = dataArray[i] / 128.0;
+        let y = v * HEIGHT/2;
+        if (i===0) {
+          canvasCtx.moveTo(x, y);
+        } else { 
+          canvasCtx.lineTo(x, y);
+        }
+        x += sliceWidth;
+      }
+      canvasCtx.lineTo(canvas.width, canvas.height/2);
+      canvasCtx.stroke();
+    };
+    return (
+      <div>
+        <canvas id="audioCanvas" width={WIDTH} height={HEIGHT}></canvas>
+        <hr/>
+        <button onClick={handleStop}>Stop Sound</button><br/>
+        <button data-freq="261" onClick={handleFreqChange}>Do</button>
+        <button data-freq="293" onClick={handleFreqChange}>Re</button>
+        <button data-freq="329" onClick={handleFreqChange}>Mi</button>
+        <button data-freq="349" onClick={handleFreqChange}>Fa</button>
+        <button data-freq="392" onClick={handleFreqChange}>So</button>
+        <button data-freq="440" onClick={handleFreqChange}>La</button>
+        <button data-freq="466" onClick={handleFreqChange}>Si</button>
+        <button data-freq="523" onClick={handleFreqChange}>Do</button><br/>
+        Wave: <select onChange={handleOscillatorType}>
+          <option>sine</option>
+          <option>square</option>
+          <option>sawtooth</option>
+          <option>triangle</option>
+        </select>
+      </div>
+    )
+  },
+  destroy: (ctx) => {
+  }
+};
+
+export default slidesGeneration(name, demoName, details, codeSamples, demo);
---
+++ src/demos/Gamepad.js
@@ -0,0 +1,220 @@
+import React from "react";
+import { slidesGeneration } from "../utils/SlidesGenerator"; 
+
+const name = "Gamepad API";
+const demoName = "gamepad";
+const details = [
+  "Processes input from a USB connected gamepad",
+  "Great for games or funky navigation in your app",
+  "Event based API"
+];
+const codeSamples = [
+  {
+    title: "Gamepad API", 
+    code: `
+window.addEventListener("gamepadconnected", (e) => {
+  console.log("Gamepad connected", e.gamepad);
+});
+  `},  
+  {
+    title: "Check button state", 
+    code: `
+const gameLoop = () => {
+  let gamepad = gamepads[0];
+  console.log(gamepad.buttons[0].pressed);
+};
+setInterval(gameLoop, 50);
+  `}
+];
+
+
+let missiles = [];
+const createMissile = (colNum) => {
+  let missile = document.createElement("span");
+  missile.classList.add("missile");
+  missiles.push(missile);
+  let row = document.querySelector("#missiles");
+  row.childNodes[colNum].appendChild(missile);
+}
+const destroyMissile = (missileIndex) => {
+  missiles[missileIndex].remove();
+  delete missiles[missileIndex];
+}
+let torpedos = [];
+let canCreateTorpedo = true;
+const fireTorpedo = (colNum) => {
+  if (canCreateTorpedo) {
+    let row = document.querySelector("#missiles");
+    let height = row.getAttribute("height");
+    let torpedo = document.createElement("span");
+    torpedo.classList.add("torpedo");
+    torpedo.style.top = -1 * height + "px";
+    torpedo.style.left = "0px";
+    torpedos.push(torpedo);
+    row.childNodes[colNum].appendChild(torpedo);
+    canCreateTorpedo = false;
+    setTimeout(() => canCreateTorpedo = true, 300);
+  }
+}
+const destroyTorpedo = (torpedoIndex) => {
+  torpedos[torpedoIndex].remove();
+  delete torpedos[torpedoIndex];
+}
+let gameStarted = false;
+
+const demo = {
+  init: (ctx) => {
+    ctx.setState({
+      score: 0,
+      gamepadBtnA: false,
+      gamepadBtnB: false,
+      gamedadBtnRight: false,
+      gamepadBtnLeft: false,
+      gamepadBtnUp: false,
+      gamepadBtnDown: false,
+      gamepadConnected: false
+    });
+  },
+  demoSocketCb: (msg, ctx) => {
+    if (!msg.name === "gamepad") return;
+    if (!gameStarted) return;
+    let col = 0;
+    switch(msg.button) {
+      case "up": col = 0; break;
+      case "down": col = 1; break;
+      case "left": col = 2; break;
+      case "right": col = 3; break;
+      case "a": col = 4; break;
+      case "b": col = 5; break;
+      default: break;
+    }
+    createMissile(col);
+  },
+  render: (ctx) => {
+    let gamepad;
+    window.addEventListener("gamepadconnected", (e) => {
+      gamepad = e.gamepad;
+      ctx.setState({gamepadConnected: true});
+      gameLoop();
+      setTimeout(() => gameStarted = true, 5000);
+    });
+    window.ongamepaddisconnected = (e) => {
+      gamepad = null;
+      ctx.setState({gamepadConnected: false});
+    }
+    const flashScreen = (colour) => {
+      let body = document.querySelector("body");
+      setTimeout(() => body.style.background = colour, 100);
+      setTimeout(() => body.style.background = "none", 200);
+      setTimeout(() => body.style.background = colour, 300);
+      setTimeout(() => body.style.background = "none", 400);
+      setTimeout(() => body.style.background = colour, 500);
+      setTimeout(() => body.style.background = "none", 600);
+    }
+    const boom = (missileIndex) => {
+      flashScreen("red");
+      destroyMissile(missileIndex);
+      ctx.setState({score: ctx.state.score - 1});
+    }
+    const lostTorpedo = (torpedoIndex) => {
+      destroyTorpedo(torpedoIndex);
+      ctx.setState({score: ctx.state.score -1});
+    }
+    const torpedoDestroysMissile = (tIndex, mIndex) => {
+      flashScreen("blue");
+      destroyMissile(mIndex);
+      destroyTorpedo(tIndex);
+      ctx.setState({score: ctx.state.score + 1});
+    }
+    const gameLoop = () => {
+      ctx.setState({
+        gamepadBtnA: gamepad.buttons[0].pressed,
+        gamepadBtnB: gamepad.buttons[1].pressed,
+        gamepadBtnRight: gamepad.axes[6] === 1,
+        gamepadBtnLeft: gamepad.axes[6] === -1,
+        gamepadBtnUp: gamepad.axes[7] === -1,
+        gamepadBtnDown: gamepad.axes[7] === 1
+      });
+      if (canCreateTorpedo) {
+        if (ctx.state.gamepadBtnUp) fireTorpedo(0);
+        if (ctx.state.gamepadBtnDown) fireTorpedo(1);
+        if (ctx.state.gamepadBtnLeft) fireTorpedo(2);
+        if (ctx.state.gamepadBtnRight) fireTorpedo(3);
+        if (ctx.state.gamepadBtnA) fireTorpedo(4);
+        if (ctx.state.gamepadBtnB) fireTorpedo(5);
+      }
+      let row = document.querySelector("#missiles");
+      let height = row.getAttribute("height");
+      missiles.map((m, index) => {
+        if (!m.style.top) m.style.top = "0px";
+        let missileTop = parseInt(m.style.top);
+        m.style.top = missileTop - 1 + "px";
+        if (missileTop < (-1 * height + 25)) {
+          boom(index);
+        }
+        return m;
+      });
+      torpedos.map((t, index) => {
+        let torpedoTop = parseInt(t.style.top);
+        t.style.top = torpedoTop + 1 + "px";
+        if(torpedoTop > 0) {
+          lostTorpedo(index);
+        }
+        missiles.map((m, mIndex) => {
+          if (t.parentNode === m.parentNode) {
+            let mTop = parseInt(m.style.top);
+            if (Math.abs(torpedoTop) - Math.abs(mTop) < 15) {
+              torpedoDestroysMissile(index, mIndex);
+            }
+          }
+          return m;
+        });
+        return t;
+      });
+      setTimeout(gameLoop, 50);
+    };
+    let gamepadBtnStyle = {
+      display: "inline-block",
+      border: "solid 2px black",
+      borderRadius: "100%",
+      width: "4em",
+      height: "4em",
+      fontSize: "2em",
+      paddingTop: "1.25em",
+      marginRight: "1em"
+    };
+    return (
+      <div className="gamepadGame">
+        <h3>Score: {ctx.state.score}</h3>
+        <table><tbody>
+        {ctx.state.gamepadConnected && 
+        <tr>
+          <td><div style={{...gamepadBtnStyle, background: ctx.state.gamepadBtnUp ? "#ff0000" : "#ffeeee"}}>^</div></td>
+          <td><div style={{...gamepadBtnStyle, background: ctx.state.gamepadBtnDown ? "#00ff00" : "#eeffee"}}>v</div></td>
+          <td><div style={{...gamepadBtnStyle, background: ctx.state.gamepadBtnLeft ? "#ffff00" : "#ffffee"}}>&lt;</div></td>
+          <td><div style={{...gamepadBtnStyle, background: ctx.state.gamepadBtnRight ? "#0000ff" : "#eeeeff"}}>&gt;</div></td>
+          <td><div style={{...gamepadBtnStyle, background: ctx.state.gamepadBtnA ? "#ff00ff" : "#ffeeff"}}>A</div></td>
+          <td><div style={{...gamepadBtnStyle, background: ctx.state.gamepadBtnB ? "#00ffff" : "#eeffff"}}>B</div></td>
+        </tr>
+        }
+        {!ctx.state.gamepadConnected && 
+        <tr><td colSpan="6">Please connect gamepad</td></tr>
+        }
+        <tr height="300" id="missiles">
+          <td valign="bottom"></td>
+          <td valign="bottom"></td>
+          <td valign="bottom"></td>
+          <td valign="bottom"></td>
+          <td valign="bottom"></td>
+          <td valign="bottom"></td>
+        </tr>
+        </tbody>
+        </table>
+      </div>
+    )
+  },
+  destroy: (ctx) => {
+  }
+};
+
+export default slidesGeneration(name, demoName, details, codeSamples, demo);
